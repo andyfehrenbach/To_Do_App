52bc2caa089ece89a29687268c94207e14bd17a0
andyfehrenbach <Andy Fehrenbach@Andy-Fehrenbachs-MacBook-Pro-2.local>
2016-02-21 19:27:56 -0600

    front end fully functional, including complete and removal event handlers. Back end stores new tasks to database.

--- public/scripts/clientapp.js
+++ public/scripts/clientapp.js
@@ -1,3 +1,6 @@
+var values = {};
+var taskData = [];
+
 $(document).ready(function() {
   getData();
   $('#tasks').on('click', showPopup);
@@ -7,7 +10,8 @@ $(document).ready(function() {
 
   $('#submit-button').on('click', sendTask);
   $('#submit-button').on('click', closePopup);
-  $('.remove').on('click', deleteTask);
+  $('.taskList').on('click', '.remove', deleteTask);
+  $('.taskList').on('click', '.complete', completeTask);
  });
 
  function showPopup () {
@@ -28,13 +32,62 @@ $(document).ready(function() {
  };
 
 function sendTask() {
+
   event.preventDefault();
-
-  var values = {};
-
   values.task_name = $('#task').val();
   console.log(values);
+  postData();
+}
 
+function getData() {
+
+    $.ajax({
+    type: 'GET',
+    url: '/task_data',
+    success: function(data) {
+      console.log(data);
+      appendTasks(data);
+      taskData.push(data);
+      console.log(taskData);
+    }
+  });
+}
+
+function deleteTask () {
+  var lineItem = $(this).task_name;
+  console.log(lineItem);
+    $(this).parent().slideUp(100);
+}
+
+function completeTask () {
+  $(this).parent().find('.lineItem').toggleClass('taskComplete');
+  $elComplete = $(this).parent().find('.itemContainer');
+  // var x = $elComplete.data('id');
+  // taskData[x].task_complete = true;
+  // console.log(taskData[x].task_complete);
+  // putData();
+$('.completedTasks').append($(this).parent());
+}
+
+
+function appendTasks(data) {
+  $('.taskList').empty();
+  for (var i = 0; i < data.length; i++) {
+    $('.taskList').append(
+      '<div class="itemContainer">'+
+        '<div class="standardHeight lineItem task">' + '<li>' + data[i].task_name + '</li>' + '</div>'+
+        '<div class="complete standardHeight lineItem">' + '<i class="material-icons">' + 'done' + '</i>' + '</div>' +
+        '<div class="remove standardHeight lineItem">' + '<i class="material-icons">' + 'clear' + '</i>' + '</div>' +
+      '</div>');
+
+      $('.itemContainer').data('id', data[i].id);
+
+  }
+  $('.taskList').append('<div class="completedTasks"></div>');
+  $('.taskList').children().last().append('<header class="standardHeight completeTask marginTop"><h1>Completed Tasks</h1></header>');
+}
+
+function postData () {
   $.ajax({
       type: 'POST',
       url: '/task_data',
@@ -47,29 +100,15 @@ function sendTask() {
     });
 }
 
-function getData() {
-    $.ajax({
-    type: 'GET',
-    url: '/task_data',
-    success: function(data) {
-      console.log(data);
-      appendDom(data);
-    }
-  });
-}
-// function deleteTask () {
-//   $(this).
-// }
-//re-useable appendDom function ONLY WORKS WITH CLASSES
+function putData () {
+  $.ajax({
+      type: 'PUT',
+      url: '/task_data/complete',
+      data: values.task_complete,
+      success: function(data) {
+        console.log('form server: task complete ' + values.task_name);
+         getData(data);
+      }
 
-function appendDom(data) {
-  $('.taskList').empty();
-  var $el = $('.taskList');
-  for (var i = 0; i < data.length; i++) {
-
-    $el.append('<div class="standardHeight lineItem task">' + '<li>' + data[i].task_name + '</li>' + '</div>').slideDown(600);
-    $el.append('<div class="complete standardHeight lineItem">' + '<i class="material-icons">' + 'done' + '</i>' + '</div>').slideDown(600);
-    $el.append('<div class="remove standardHeight lineItem">' + '<i class="material-icons">' + 'clear' + '</i>' + '</div>').slideDown(600);
-
-  }
+    });
 }
